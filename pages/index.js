fafa78e707940278c20493d3f0cf08add8de7896
unknown <pareshchawla.56@gmail.com>
2023-01-02 19:04:38 +0530

    Modify index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import deved from "../public/pareshbitmoji2.png";
+import profile from "../public/pareshbitmoji2.png";
 import android from "../public/android.png";
 import website from "../public/website.png";
 import blockchain from "../public/block.png";
@@ -63,7 +63,7 @@ export default function Home() {
             <a href="https://www.hackerrank.com/pareshchawla" target="_blank" rel="noreferrer"><AiFillCode /></a>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" alt="" />
+              <Image src={profile} layout="fill" objectFit="cover" alt="" />
             </div>
           </div>
         </section>
